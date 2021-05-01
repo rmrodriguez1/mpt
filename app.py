20635b16905b5fa21ba9525fba95338120b37dd9
Rosario <rmrodriguez1@uc.cl>
2021-04-30 20:24:35 -0400

    p

--- app.py
+++ app.py
@@ -416,7 +416,7 @@ api.add_resource(Play_Track, '/tracks/<string:track_id>/play')
 
 @app.route('/')
 def index():
-    return "<h1>Tarea 2 - Taller de integración</h1><br><img src='https://www.killyourdarlings.com.au/wp-content/uploads/2017/04/pusheen4.gif'>"
+    return "<div style="margin: 10px auto 20px; display: block;"><img src='https://www.killyourdarlings.com.au/wp-content/uploads/2017/04/pusheen4.gif'></div>"
 
 if __name__=='__main__':
 
