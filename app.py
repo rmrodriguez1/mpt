42cfb841f40e64a08fa424d66ffb9b008a3a1d03
Rosario <rmrodriguez1@uc.cl>
2021-05-02 21:33:39 -0400

    k

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ class All_Artists(Resource):
     def get(self): 
         return list(map(lambda x: x.json(), Artists.query.all())), 200
 
-def post(self): 
+    def post(self): 
 
         args = All_Artists.parser.parse_args()
         if not args['name'] or not args['age']:
